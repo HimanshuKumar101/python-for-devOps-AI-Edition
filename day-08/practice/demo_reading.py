b9f57954abc023334e539aa2389be65a8f4121c9
Himanshu-dev001 <himanshu.kumar@hastree.com>
2026-01-05 23:03:18 +0530

    analyzer log oops

---
+++ day-08/practice/demo_reading.py
@@ -0,0 +1,8 @@
+from utilities import read_file # importing the package
+# Abstraction
+
+print(read_file("app.log"))
+
+
+# modular code
+# this code can be used in other files by importing this package 
---
+++ day-08/practice/utilities.py
@@ -0,0 +1,16 @@
+"""
+Packages:
+ are python functions that can be imported 
+ in some other code file
+"""
+import json
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        return file.readlines()
+    
+def write_json(filename,json_object):
+    with open(filename, "w+") as file:
+        json.dump(json_object,file)
+
+        # these functions can be used in other files by importing this package 
